d8b3440e4969fee1658810396d8f6a48db5af7b4
christian-bromann <contact@christian-bromann.com>
2014-03-19 01:07:46 +0100

    implemented base functionality: save part of webpage screenshot as image

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * WebdriverCSS
+ * Regression testing tool for WebdriverJS
+ * 
+ * @author Christian Bromann <mail@christian-bromann.com>
+ * @license Licensed under the MIT license.
+ */
+
+module.exports.init = require('./lib/webdrivercss.js');
