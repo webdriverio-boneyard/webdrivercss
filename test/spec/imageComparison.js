bf8e3a21443dc257e8c7aa3bea279ae0197df058
christian-bromann <contact@christian-bromann.com>
2014-10-20 03:07:09 +0200

    add test to check if updateBaseline works

--- test/spec/imageComparison.js
+++ test/spec/imageComparison.js
@@ -115,10 +115,59 @@ describe('WebdriverCSS compares images and exposes information about CSS regress
 
     });
 
-    describe('should take a screenshot of same area without any changes in it again', function(done) {
+    describe('should take a screenshot of same area without any changes in it', function() {
         var resultObject = {};
 
         before(function(done) {
+            this.browser
+                .webdrivercss('comparisonTest', capturingData, function(err,res) {
+                    should.not.exist(err);
+                    resultObject = res[capturingData.name][0];
+                })
+                .call(done);
+        });
+
+        it('should exist an image (*.baseline.png) in the default image folder', function(done) {
+            fs.exists(resultObject.baselinePath, function(exists) {
+                exists.should.equal(true);
+                done();
+            });
+        });
+
+        it('should exist an image (*.regression.png) in the default image folder', function(done) {
+            fs.exists(resultObject.regressionPath, function(exists) {
+                exists.should.equal(true);
+                done();
+            });
+        });
+
+        it('should exist an image (*.diff.png) in the default failed comparisons image folder', function(done) {
+            fs.exists(resultObject.diffPath, function(exists) {
+                exists.should.equal(true);
+                done();
+            });
+        });
+
+        it('should exist an *.diff image with same dimension', function() {
+            resultObject.isSameDimensions.should.be.a('boolean');
+            resultObject.isSameDimensions.should.equal(true);
+        });
+
+        it('should have an mismatch percentage of 35.65%', function() {
+            resultObject.misMatchPercentage.should.be.a('number');
+            resultObject.misMatchPercentage.should.equal(35.65);
+            resultObject.isExactSameImage.should.equal(false);
+            resultObject.isWithinMisMatchTolerance.should.equal(false);
+        });
+
+    });
+
+    describe('updates baseline if updateBaseImages is given', function(done) {
+        var resultObject = {};
+
+        before(function(done) {
+            capturingData.updateBaseline = true;
+
             this.browser
                 .webdrivercss('comparisonTest', capturingData, function(err,res) {
                     should.not.exist(err);
